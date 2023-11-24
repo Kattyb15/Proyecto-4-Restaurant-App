21da8db48f618d9abe3bd1e45345bea153bd2dc9
katherine delgadillo rosas <140263889+Kattyb15@users.noreply.github.com>
2023-11-24 12:29:49 -0600

    Inicio

---
+++ app.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <ul>
+        <li><a href="#inicio">Inicio</a></li>
+        <li><a href="#menú">Menú</a></li>
+        <li><a href="#reservación">Reservación</a></li>
+      </ul>
+    </>
+  )
+}
+
+export default App
